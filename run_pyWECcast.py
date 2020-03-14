e955f4c8477a5e5cfad7dadc83a330a36f7bfd6f
aidanbharath <Aidan.Bharath@nrel.gov>
2020-03-14 13:51:46 -0600

    can reduce things now

--- run_pyWECcast.py
+++ run_pyWECcast.py
@@ -8,5 +8,7 @@ modelRegions = ['wc_10m']
 #wc.models.processH5('t00z',search_host=False)
 
 db = f'./model_downloads.h5'
-ww = wc.WW3(db)
-ww.get_location(1,2)
+ww = wc.WW3(db,235,46)
+ww.get_location()
+ww.get_times('2020-03-11','2020-03-12')
+ww.reduce_db('test.h5')
