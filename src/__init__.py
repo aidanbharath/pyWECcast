90e6505caa56116e17f929e14e804df74d48426f
aidanbharath <Aidan.Bharath@nrel.gov>
2020-07-28 12:20:53 -0600

    freash day

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,6 @@ from .NOAA import buoys,models
 from .power import link_sea_states, calculate_fft_matrix, construct_powerseries
 from .spreading import wec_direction, wec_window, wave_window
 from .arrays import multiple_realizations
-from .wecSim import wecSimFuns
+from .wecSim import wecsim_mats_to_hdf
 
 from .ufuncs import *
