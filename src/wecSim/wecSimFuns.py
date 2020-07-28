89ca1fd42d66f0482f19e550248e175ab2281bac
aidanbharath <Aidan.Bharath@nrel.gov>
2020-07-28 14:59:46 -0600

    fixed Davids fix

--- src/wecSim/wecSimFuns.py
+++ src/wecSim/wecSimFuns.py
@@ -106,7 +106,7 @@ def wecsim_mats_to_hdf(wecSimDatDir, modelName, outputDir=None, compression=None
         Hs = round(mat[top_label][H_label][0,0],2)
         Tp = round(mat[top_label][T_label][0,0],2)
         power = mat[top_label][power_label][0,:]
-        time = mat[ref_label][F_label][time_label][0,:]
+        time = mat[top_label][time_label][0,:]
         with h5.File(dbName, 'a') as hdf:
             hdf.create_dataset(f'Seed_{seed}/Hs_{Hs}/Tp_{Tp}/{varName}', data=power*power_multiplier,
                                compression=compression) # N.B. -1
