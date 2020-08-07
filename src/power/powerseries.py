98003468863c9b159af598bd850c98e75017a9ae
dav-og <12831266+dav-og@users.noreply.github.com>
2020-08-06 21:09:22 -0600

    Update powerseries.py
    - removed formatters from 'label' f-string in calculate_fft_matrix()
    - removed whitespace

--- src/power/powerseries.py
+++ src/power/powerseries.py
@@ -3,7 +3,7 @@ import warnings
 
 from h5py import File
 from numpy import array, abs, zeros, zeros_like, unique, pi, cos, sin, sum, newaxis, vstack, hstack, where, equal, all, nan
-from numpy import int64 as i64 
+from numpy import int64 as i64
 from numpy import float64 as f64
 from numpy.ma import masked_equal
 from scipy.spatial import cKDTree
@@ -18,11 +18,10 @@ from tqdm import tqdm
 @njit(['float64[:,:](complex128[:,:],float64[:],float64[:],float64[:,:],int64,int64,b1)'],
          nogil=True,parallel=True)
 def __reconstruction__(spectrum,frequency,times,result,lntime,nWECs,inPhase):
-    
     """
         Parameters
         ----------
-        spectrum : complex128 ndarray[:,:] (frequency_components, direction) 
+        spectrum : complex128 ndarray[:,:] (frequency_components, direction)
             Scaled FFT components in the desired directions
         frequency : float64 ndarray[:] (frequencies)
             frequency values associated with the spectrum
@@ -30,18 +29,18 @@ def __reconstruction__(spectrum,frequency,times,result,lntime,nWECs,inPhase):
             times values associated with the reconstruction
         result : float64 ndarray[:] (reconstrustion,direction)
             result array filled during function execution
-        lntime : int64 int 
+        lntime : int64 int
             length of the time output array
         ndeg : int64 int
             length of the direction output array
-        inPhase : boolean 
+        inPhase : boolean
             choose if all reconstructions should be in phase or not
-            
+
         Returns:
             result : float64 ndarray[:,:] (times,direction)
                 reconstruction array
     """
-   
+
     for j in range(nWECs):
         if not inPhase:
             rInt = randint(0,1e8)
@@ -53,16 +52,15 @@ def __reconstruction__(spectrum,frequency,times,result,lntime,nWECs,inPhase):
             s = spectrum[:,j]
             result[i,j] = sum(s.real*Cos+s.imag*Sin)
     return result
-   
+
 
 @njit(['float64[:,:](complex128[:,:],float64[:],float64[:],float64[:,:],int64,int64,int64[:])'],
          nogil=True,parallel=True)
 def __reconstruction_seed__(spectrum,frequency,times,result,lntime,nWECs,Seed):
-    
     """
         Parameters
         ----------
-        spectrum : complex128 ndarray[:,:] (frequency_components, direction) 
+        spectrum : complex128 ndarray[:,:] (frequency_components, direction)
             Scaled FFT components in the desired directions
         frequency : float64 ndarray[:] (frequencies)
             frequency values associated with the spectrum
@@ -70,18 +68,18 @@ def __reconstruction_seed__(spectrum,frequency,times,result,lntime,nWECs,Seed):
             times values associated with the reconstruction
         result : float64 ndarray[:] (reconstrustion,direction)
             result array filled during function execution
-        lntime : int64 int 
+        lntime : int64 int
             length of the time output array
         ndeg : int64 int
             length of the direction output array
-        Seed : int64 ndarray[:] 
+        Seed : int64 ndarray[:]
             choose if all reconstructions should be in phase or not
-            
+
         Returns:
             result : float64 ndarray[:,:] (times,direction)
                 reconstruction array
     """
-   
+
     for j in range(nWECs):
         seed(Seed[j])
         rInt = randint(0,1e8)
@@ -91,12 +89,12 @@ def __reconstruction_seed__(spectrum,frequency,times,result,lntime,nWECs,Seed):
             s = spectrum[:,j]
             result[i,j] = sum(s.real*Cos+s.imag*Sin)
     return result
-    
-    
+
+
 def link_sea_states(WECSim,Hs,Tp,Dir=None,kdTree=False,Seed=None):
     """
     Parameters:
-    
+
     WECSim : str
         filename associated with the WECSim powerseries matrix
     Hs : float64 ndarray[:]
@@ -107,12 +105,12 @@ def link_sea_states(WECSim,Hs,Tp,Dir=None,kdTree=False,Seed=None):
         array of wave direction from the sea-state data
     kdTree : optional boolean default:False
         option to use kdtree method for finding nearest neighbours
-        
+
     Returns:
         ndarray : (WECSim_Hs,WECSim_Tp)
-            Hs and Tp values corresponding to available simulation in powerseries matrix 
+            Hs and Tp values corresponding to available simulation in powerseries matrix
     """
-    
+
     def __find_shortest__(values,wsHs,wsTp,wsseeds,Seed):
         wsA = array([array(wsHs), array(wsTp), array(wsseeds)]).T
         Hs, Tp, seedlist = [], [], []
@@ -138,10 +136,10 @@ def link_sea_states(WECSim,Hs,Tp,Dir=None,kdTree=False,Seed=None):
                     Hs.append(wsA_seed[0])
                     Tp.append(wsA_seed[1])
                     seedlist.append(wsA_seed[2])
-                    
+
         return array([Hs,Tp]), array(seedlist)
-            
-    
+
+
     if not Dir:
         wsHs, wsTp, wsseedlist = [], [], []
         with File(WECSim,'r') as pMatrix:
@@ -152,9 +150,9 @@ def link_sea_states(WECSim,Hs,Tp,Dir=None,kdTree=False,Seed=None):
                             wsHs.append(float(hs[3::]))
                             wsTp.append(float(tp[3::]))
                             wsseedlist.append(float(s[5::]))
-                                    
-            
-        values = array([Hs,Tp]).T        
+
+
+        values = array([Hs,Tp]).T
         if kdTree:
             tree = cKDTree(seas)
             _, idx = tree.query(values)
@@ -163,17 +161,17 @@ def link_sea_states(WECSim,Hs,Tp,Dir=None,kdTree=False,Seed=None):
         else:
             result, seedlist = __find_shortest__(values,wsHs,wsTp,wsseedlist,Seed)
             return result.T, seedlist, values
-            
-            
+
+
     elif Dir:
         pass
 
-    
+
 def calculate_fft_matrix(WECSim, Hs, Tp, seedlist, Dir=None, fftFname=f'./tempFFT.h5', inMemory=False,
                         WS_time=f'Time',WS_variable=f'Power',cutoff=None):
     """
     Parameters:
-    
+
     WECSim : str
         filename associated with the WECSim powerseries matrix
     Hs : float64 ndarray[:]
@@ -195,18 +193,18 @@ def calculate_fft_matrix(WECSim, Hs, Tp, seedlist, Dir=None, fftFname=f'./tempFF
         WECSim variable name to use in FFT
     cutoff : optional int
         Number of values to remove for the start of the timeseries
-        
+
     Returns:
         dict : (coefficients complex128 ndarray[:], frequencies float64 ndarray[:])
             if inMemory:True ; returns FFT results
     """
-    
+
     if inMemory: results = {}
-    
+
     if not Dir:
         uni = unique([i for i in zip(Hs,Tp,seedlist)],axis=0)
         for i in range(uni.shape[0]):
-            label = f'Hs_{uni[i,0]:.2f}/Tp_{uni[i,1]:.2f}/Seed_{uni[i,2]:.1f}'
+            label = f'Hs_{uni[i,0]}/Tp_{uni[i,1]}/Seed_{uni[i,2]}'
             with File(WECSim,'r') as ws:
                 time = ws[WS_time][cutoff:]
                 variable = ws[label][WS_variable][cutoff:]
@@ -219,21 +217,20 @@ def calculate_fft_matrix(WECSim, Hs, Tp, seedlist, Dir=None, fftFname=f'./tempFF
                     FFT.create_dataset(f'{label}/coefficients', data=coefs, dtype=coefs.dtype)
             else:
                 results[f'{label}/frequency'] = fftfreq(time.shape[0],d=time[1]-time[0])
-                results[f'{label}/coefficients'] = fft(variable) 
-                    
+                results[f'{label}/coefficients'] = fft(variable)
+
     else:
         pass
-    
+
     if inMemory:
         return results
-    
-    
+
+
 def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'./tempFFT.h5',
                          recFile=f'./tempRecon.h5',inMemory=False,inPhase=False,WSseed=None):
-       
     """
     Parameters:
-    
+
     timestamps : datetimeIndex
         timestamps associated with the reconstruction times
     freq : str
@@ -259,20 +256,20 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
         parameter to generate the reconstruction with fixed or random phase values
     WSseed : optional int
         value to specify what seed to use when choosing WECSim seeds
-        
+
     Returns:
         dict : (coefficients complex128 ndarray[:], frequencies float64 ndarray[:])
             if inMemory:True ; returns FFT results
-    """             
-    
-    
-    for i in tqdm(range(timestamps.shape[0]-1)):    
+    """
+
+
+    for i in tqdm(range(timestamps.shape[0]-1)):
         times = date_range(timestamps[i], timestamps[i+1], freq=freq)[:-1]
         intTimes = array(times.astype(i64)/10**9)
         lnTime = intTimes.shape[0]
         ffts = f'Hs_{Hs[i]:.2f}/Tp_{Tp[i]:.2f}/Seed_{seedlist[i]:.1f}/coefficients'
         freqs = f'Hs_{Hs[i]:.2f}/Tp_{Tp[i]:.2f}/Seed_{seedlist[i]:.1f}/frequency'
-        
+
         if Dir is None:
             if type(fft_matrix) is not type(''):
                 coefs, f = fft_matrix[ffts], fft_matrix[freqs]
@@ -285,7 +282,7 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
                 construct = N*__reconstruction__(coefs,f,intTimes,construct,lnTime,cShape,inPhase)
                 #else:
                 #    construct = N*__reconstruction_seed__(coefs,f,intTimes,construct,lnTime,cShape,Seeds)
-        
+
             else:
                 with File(fft_matrix,'r') as ws:
                     coefs, f = ws[ffts][:], ws[freqs][:]
@@ -298,7 +295,7 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
                 construct = N*__reconstruction__(coefs,f,intTimes,construct,lnTime,cShape,inPhase)
                 #else:
                 #    construct = N*__reconstruction_seed__(coefs,f,intTimes,construct,lnTime,cShape,Seeds)
-                    
+
         else:
             if type(fft_matrix) is not type(''):
                 coefs, f = fft_matrix[ffts], fft_matrix[freqs]
@@ -312,7 +309,7 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
                 construct = N*__reconstruction__(coefs,f,intTimes,construct,lnTime,cShape,inPhase)
                 #else:
                 #    construct = N*__reconstruction_seed__(coefs,f,intTimes,construct,lnTime,cShape,Seeds)
-                    
+
             else:
                 with File(fft_matrix,'r') as ws:
                     coefs, f = ws[ffts][:], ws[freqs][:]
@@ -326,7 +323,7 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
                 construct = N*__reconstruction__(coefs,f,intTimes,construct,lnTime,cShape,inPhase)
                 #else:
                 #    construct = N*__reconstruction_seed__(coefs,f,intTimes,construct,lnTime,cShape,Seeds)
-        
+
         if i == 0:
             if not inMemory:
                 with File(recFile,'a') as recon:
@@ -337,7 +334,7 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
             else:
                 results = construct
                 time = times
-            
+
         else:
             if not inMemory:
                 with File(recFile, 'a') as recon:
@@ -349,7 +346,6 @@ def construct_powerseries(timestamps,freq,Hs,Tp,seedlist,Dir=None,fft_matrix=f'.
             else:
                 results = vstack([results,construct])
                 time = hstack([time, times])
-                
-        
+
+
     if inMemory: return results, time
-            
